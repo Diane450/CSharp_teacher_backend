74d0b7e717daf6cc9ff571a951b1085a405b9c27
Diane450 <pocket2222@mail.ru>
2026-08-18 17:49:03 +0400

    add logout

--- CSharp_teacher/Controllers/AuthorizationController.cs
+++ CSharp_teacher/Controllers/AuthorizationController.cs
@@ -6,6 +6,7 @@ using CSharp_teacher.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CSharp_teacher.Controllers
 {
@@ -16,7 +17,7 @@ namespace CSharp_teacher.Controllers
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
         private readonly TokenService _tokenService;
-        private readonly AppDbContext _appDbContext;
+        private readonly AppDbContext _context;
         private readonly IConfiguration _configuration;
         public AuthorizationController(UserManager<User> userManager,
                                         SignInManager<User> signInManager,
@@ -28,7 +29,7 @@ namespace CSharp_teacher.Controllers
             _signInManager = signInManager;
             _tokenService = tokenService;
             _configuration = configuration;
-            _appDbContext = appDbContext;
+            _context = appDbContext;
         }
 
         [HttpPost("register")]
@@ -69,14 +70,14 @@ namespace CSharp_teacher.Controllers
 
             var refreshToken = new RefreshToken
             {
-                Token = accessToken,
+                Token = refreshTokenString,
                 UserId = user.Id,
                 CreatedAt = DateTime.UtcNow,
                 ExpiresAt = DateTime.UtcNow.AddDays(refreshTokenDays),
                 IsRevoked = false,
             };
-            _appDbContext.RefreshTokens.Add(refreshToken);
-            await _appDbContext.SaveChangesAsync();
+            _context.RefreshTokens.Add(refreshToken);
+            await _context.SaveChangesAsync();
             var cookieOptions = new CookieOptions
             {
                 HttpOnly = true,
@@ -92,11 +93,33 @@ namespace CSharp_teacher.Controllers
             });
         }
 
+        [HttpPost("logout")]
+        public async Task<IActionResult> LogOut()
+        {
+            if (!Request.Cookies.TryGetValue("refreshToken", out var refreshTokenString))
+            {
+                return Ok(new { message = "Вы уже вышли из системы"});
+            }
+
+            var existingToken = await _context.RefreshTokens
+                    .Include(rt => rt.User)
+                    .FirstOrDefaultAsync(rt => rt.Token == refreshTokenString);
+
+            if (existingToken != null)
+            {
+                existingToken.IsRevoked = true;
+                existingToken.RevokedAt = DateTime.UtcNow;
+                await _context.SaveChangesAsync();
+            }
+            Response.Cookies.Delete("refreshToken");
+            return Ok(new { message = "Вы успешно вышли из системы" });
+        }
+
         [Authorize]
-        [HttpPost]
+        [HttpGet("test")]
         public IActionResult Test()
         {
-            return Ok(new { message = "Ok" });
+            return Ok();
         }
     }
 }
